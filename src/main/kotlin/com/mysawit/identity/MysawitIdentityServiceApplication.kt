a4f6c46dc629e2c0e02130b3a2fa803c5de67b50
azzelll <madeshandykrisnanda@gmail.com>
2026-02-17 23:50:56 +0700

    fix issue

--- src/main/kotlin/com/mysawit/identity/MysawitIdentityServiceApplication.kt
+++ src/main/kotlin/com/mysawit/identity/MysawitIdentityServiceApplication.kt
@@ -7,5 +7,8 @@ import org.springframework.boot.runApplication
 class MysawitIdentityServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<MysawitIdentityServiceApplication>(*args)
+    val context = runApplication<MysawitIdentityServiceApplication>(*args)
+    if (context.environment.getProperty("app.test.close-context", Boolean::class.java, false)) {
+        context.close()
+    }
 }
