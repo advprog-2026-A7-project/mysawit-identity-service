01452edac24fa5aae21ca4e175abc9c7267a9f54
FalahHadziqul <hadziqulfalah18@gmail.com>
2026-04-12 20:12:23 +0700

    feat: RabbitMQ event publishing

--- src/test/java/com/mysawit/identity/integration/BaseIntegrationTest.java
+++ src/test/java/com/mysawit/identity/integration/BaseIntegrationTest.java
@@ -1,10 +1,12 @@
 package com.mysawit.identity.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,4 +21,7 @@ public abstract class BaseIntegrationTest {
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @MockitoBean
+    protected RabbitTemplate rabbitTemplate;
 }
